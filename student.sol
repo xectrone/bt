eb538773e6181aa4d9231cf690a16eea439ffc8c
Bhushan Malekar <84770287+xectrone@users.noreply.github.com>
2024-11-03 14:05:32 +0530

    Changes to be committed: 	modified:   student.sol 	modified:   testbank.sol

--- student.sol
+++ student.sol
@@ -1,11 +1,8 @@
-// SPDX-License-Identifier: MIT   
+// SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.7;
 
-// Build the Contract
 contract MarksManagmtSys
 {
-	// Create a structure for
-	// student details
 	struct Student
 	{
 		int ID;
@@ -23,31 +20,23 @@ contract MarksManagmtSys
 		require(owner == msg.sender);
 		_;
 	}
+
 	constructor()
 	{
 		owner=msg.sender;
 	}
 
-	// Create a function to add
-	// the new records
 	function addNewRecords(int _ID,
 						string memory _fName,
 						string memory _lName,
 						int _marks) public onlyOwner
 	{
-		// Increase the count by 1
 		stdCount = stdCount + 1;
-
-		// Fetch the student details
-		// with the help of stdCount
-		stdRecords[stdCount] = Student(_ID, _fName,
-									_lName, _marks);
+		stdRecords[stdCount] = Student(_ID, _fName,_lName, _marks);
 	}
 
-	// Create a function to add bonus marks
 	function bonusMarks(int _bonus) public onlyOwner
 	{
-		stdRecords[stdCount].marks =
-					stdRecords[stdCount].marks + _bonus;
+		stdRecords[stdCount].marks = stdRecords[stdCount].marks + _bonus;
 	}
 }
